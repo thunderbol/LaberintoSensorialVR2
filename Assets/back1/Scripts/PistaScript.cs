784204061cbe977cab98a4639e86ea10b64f226a
Thokora <fawergilthokora@hotmail.com>
2018-05-15 03:29:36 -0500

    Commit_Fawer 15/05/2018 3:29 am

--- Assets/back1/Scripts/PistaScript.cs
+++ Assets/back1/Scripts/PistaScript.cs
@@ -8,16 +8,18 @@ public class PistaScript : MonoBehaviour {
     public GameObject BrazoAll;
     public GameObject player;
     public GameObject[] Pistas;
-    //public Animator[] animPist;
+    public Animator[] animPist;
+    Collider ColPista;
     bool Comprobador;
     float ValorRot;
 
     void Start()
     {
-        /*animPist[0] = Pistas[0].GetComponent<Animator>();
+        animPist[0] = Pistas[0].GetComponent<Animator>();
         animPist[1] = Pistas[1].GetComponent<Animator>();
         animPist[2] = Pistas[2].GetComponent<Animator>();
-        animPist[3] = Pistas[3].GetComponent<Animator>();*/
+        animPist[3] = Pistas[3].GetComponent<Animator>();
+        ColPista = Pistas[0].GetComponent<Collider>();
 
         Comprobador = false;
     }
@@ -32,14 +34,14 @@ public class PistaScript : MonoBehaviour {
         {
             Comprobador = false;
             Pistas[0].SetActive(false);
-            //animPist[0].SetBool("Mostrar", false);
-
+            animPist[0].SetBool("Mostrar", false);
+            ColPista.enabled = false;
         }
         else if (Comprobador == false)
         {
             Pistas[0].SetActive(true);
-            //animPist[0].SetBool("Mostrar", true);
-            StartCoroutine(EsperarPistas());
+            animPist[0].SetBool("Mostrar", true);
+            StartCoroutine(EsperarCollider());
         }
 
         if (Comprobador == false)
@@ -47,9 +49,9 @@ public class PistaScript : MonoBehaviour {
             Pistas[1].SetActive(false);
             Pistas[2].SetActive(false);
             Pistas[3].SetActive(false);
-            /*animPist[1].SetBool("Mostrar", false);
+            animPist[1].SetBool("Mostrar", false);
             animPist[2].SetBool("Mostrar", false);
-            animPist[3].SetBool("Mostrar", false);*/
+            animPist[3].SetBool("Mostrar", false);
         }
 
     }
@@ -65,35 +67,47 @@ public class PistaScript : MonoBehaviour {
 
     IEnumerator EsperarPistas()
     {
-        yield return new WaitForSecondsRealtime(1.0f);
+        yield return new WaitForSecondsRealtime(2.0f);
         if (Comprobador == true)
         {
             Pistas[0].SetActive(false);
             Pistas[1].SetActive(true);
             Pistas[2].SetActive(true);
             Pistas[3].SetActive(true);
-            /*animPist[1].SetBool("Mostrar", true);
+            animPist[1].SetBool("Mostrar", true);
             animPist[2].SetBool("Mostrar", true);
-            animPist[3].SetBool("Mostrar", true);*/
+            animPist[3].SetBool("Mostrar", true);
         }
+    }
+
+    IEnumerator EsperarCollider()
+    {
+        yield return new WaitForSecondsRealtime(0.4f);
+        ColPista.enabled = true;
     }
 
     public void MostrarPistas1()
     {
+        Debug.Log("Mostrar Pista1");
     }
     public void NoMostrarPistas1()
     {
+        Debug.Log("No Mostrar Pista2");
     }
     public void MostrarPistas2()
     {
+        Debug.Log("Mostrar Pista2");
     }
     public void NoMostrarPistas2()
     {
+        Debug.Log("No Mostrar Pista2");
     }
     public void MostrarPistas3()
     {
+        Debug.Log("Mostrar Pista3");
     }
     public void NoMostrarPistas3()
     {
+        Debug.Log("No Mostrar Pista3");
     }
 }
